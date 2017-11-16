aa578d704ade4e92413d497bac85306685f0590e
luohaisi <317877262@qq.com>
2017-11-16 18:03:40 +0800

    add login page

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, Button, TouchableOpacity, WebView } from 'react-native';
 
+import LoginScreen from './Pages/LoginScreen';
 import HomeScreen from './Pages/HomeScreen';
 import WebScreen from './Pages/WebScreen';
 
@@ -8,6 +9,7 @@ import { StackNavigator } from 'react-navigation';
 
 // 路由导航
 const App = StackNavigator({
+  Login: { screen: LoginScreen },
   Home: { screen: HomeScreen },
   Web:{screen: WebScreen}
 });
---
+++ Pages/LoginScreen.js
@@ -0,0 +1,201 @@
+import React, { Component } from 'react';
+import { 
+  AppRegistry, 
+  StyleSheet, 
+  Text, 
+  TextInput, 
+  View, 
+  Image, 
+  Modal,
+  TouchableHighlight
+
+} from 'react-native';
+
+import { Button, InputItem, WhiteSpace, Toast } from 'antd-mobile';
+
+export default class LoginScreen extends Component {
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: '',
+      password: '',
+      modalVisible: false,
+    };
+  }
+
+  submit = () =>{
+
+    Toast.loading('Loading...', 1, () => {
+
+      // console.log('Load complete !!!');
+      this.setModalVisible(true)
+    });
+
+    
+
+    // fetch('https://facebook.github.io/react-native/movies.json')
+    // .then((response) => response.json())
+    // .then((responseJson) => {
+    //   // alert(responseJson.movies[0].title);
+    //   alert(this.state.password);
+    // })
+    // .catch((error) => {
+    //   console.error(error);
+    // });
+    
+  }
+
+  static navigationOptions = {
+    header: null,
+  };
+
+  render() {
+
+    const { navigate } = this.props.navigation;
+
+    return (
+      // Try setting `alignItems` to 'flex-start'
+      // Try setting `justifyContent` to `flex-end`.
+      // Try setting `flexDirection` to `row`.
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor:'rgb(19,125,188)'
+      }}>
+        <View style={{marginTop:50}} >
+          <Image source={require('../Images/Icons/hat.png')} />
+        </View>
+        <View style={{alignItems:'center'}} >
+          <Text style={{color:'#F2F2F2',fontSize:20}}>绿智汇阳光采购云平台</Text>
+          <WhiteSpace/>
+          <Text style={{color:'#ccc',fontSize:16}}>Welcome To Our App</Text>
+        </View>
+        <View style={{width: '80%'}}>          
+
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(account) => this.setState({account})}
+            placeholder={'手机/邮箱/账号'}
+            underlineColorAndroid="rgb(45,155,212)"
+          />
+
+          <WhiteSpace/>
+
+          <TextInput
+            secureTextEntry={true}
+            style={styles.textInput}
+            onChangeText={(password) => this.setState({password})}
+            placeholder={'密码'}
+            underlineColorAndroid="rgb(45,155,212)"
+          />
+
+          <WhiteSpace size='xl' />
+
+          <Button type="primary" inline onClick={this.submit} style={styles.submitButton} color="#f2f2f2">登入</Button>
+
+          <WhiteSpace/>
+
+          <View style={{height: 50}}>
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+              <View style={{width: '50%', height: 50, alignItems:'center'}}>
+                <Text style={{color:'#ccc'}}>注册</Text>
+              </View>
+              <View style={{width: '50%', height: 50, alignItems:'center'}}>
+                <Text style={{color:'#ccc'}}>忘记密码?</Text>
+              </View>
+            </View>
+          </View>
+
+        </View>        
+
+        <View style={{height: 100}} >
+          
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+            <View style={{width: '15%', height: 50, alignItems:'center'}}>
+              <Image source={require('../Images/Icons/weibo.png')} style={{width:32,height:32}} />
+            </View>
+            <View style={{width: '15%', height: 50, alignItems:'center'}}>
+              <Image source={require('../Images/Icons/wechat.png')} style={{width:32,height:32}} />
+            </View>
+            <View style={{width: '15%', height: 50, alignItems:'center'}}>
+              <Image source={require('../Images/Icons/alipay.png')} style={{width:32,height:32}} />
+            </View>
+          </View>
+        </View>
+
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {alert("Modal has been closed.")}}
+          >
+
+          <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+            <View style={{width: 200, height: 50, backgroundColor: 'powderblue'}}>
+              <Text>用户名: {this.state.account}</Text>
+            </View>
+            <View style={{width: 200, height: 50, backgroundColor: 'skyblue'}}>
+              <Text>密码: {this.state.password}</Text>
+            </View>
+            <View style={{width: 200, height: 50}}>
+              <TouchableHighlight style={styles.signInHome} onPress={() => {
+                this.setModalVisible(!this.state.modalVisible)
+                navigate('Home')
+              }}>
+                <Text style={{fontSize:20,color:'lightblue',alignSelf:'center'}}>进入首页</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  textInput:{
+    height: 50, 
+    backgroundColor: 'rgb(45,155,212)',
+    paddingLeft: 20,
+    color:'#F2F2F2',
+    fontSize:16
+    // borderColor: 'gray', 
+    // borderWidth: 1
+  },
+  submitButton:{
+    height: 50, 
+    backgroundColor: 'rgb(19,125,188)',
+    borderColor:'rgb(131,194,148)',
+    borderRadius:50,
+  },
+  signInHome:{
+    width: 200, 
+    height: 50, 
+    backgroundColor:'#f2f2f2',
+  },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('IGreenBuy', () => LoginScreen);
